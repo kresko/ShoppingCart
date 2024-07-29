b42bbda530bb045bdaeefad1d28f3bbc3aa7fe99
kresko <kresko1995@gmail.com>
2024-07-29 21:30:41 +0200

    Adding navigation component.
    Adding navigation component.

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -3,14 +3,16 @@ import './Navigation.css';
 function Navigation() {
     return (
         <>
-            <h1>Navigation</h1>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Products</a></li>
-            </ul>
+            <div className="navigation">
+                <h1>Navigation</h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                </ul>
 
-            <div className="add-to-cart-button-container">
-                <button className="add-to-cart-button">Add to cart</button>
+                <div className="add-to-cart-button-container">
+                    <button className="add-to-cart-button">Add to cart</button>
+                </div>
             </div>
         </>
     );
